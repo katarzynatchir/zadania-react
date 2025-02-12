58aa4dea8149301b8472162d173f9e5bca9abe33
Katarzyna Tchir <tchir.katarzyna@gmail.com>
2025-02-12 15:17:22 +0100

    Rozwiązane

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,22 +1,33 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [showInfo, setShowInfo] = useState('mars');
+
   return (
     <>
       <h1>Wspaniałe fakty o planetach</h1>
-      <button>Mars</button>
-      <button>Jowisz</button>
-      <button>Saturn</button>
-      <section>
-        <h2>Mars</h2>
-        <p>Mars jest znaną jako Czerwona Planeta.</p>
-      </section>
-      <section>
-        <h2>Jowisz</h2>
-        <p>Jowisz jest największą planetą w naszym układzie słonecznym.</p>
-      </section>
-      <section>
-        <h2>Saturn</h2>
-        <p>Saturn słynie z pięknych pierścieni.</p>
-      </section>
+      <button onClick={() => setShowInfo('mars')}>Mars</button>
+      <button onClick={() => setShowInfo('jowisz')}>Jowisz</button>
+      <button onClick={() => setShowInfo('saturn')}>Saturn</button>
+
+      {showInfo === 'mars' && (
+        <section>
+          <h2>Mars</h2>
+          <p>Mars jest znaną jako Czerwona Planeta.</p>
+        </section>
+      )}
+      {showInfo === 'jowisz' && (
+        <section>
+          <h2>Jowisz</h2>
+          <p>Jowisz jest największą planetą w naszym układzie słonecznym.</p>
+        </section>
+      )}
+      {showInfo === 'saturn' && (
+        <section>
+          <h2>Saturn</h2>
+          <p>Saturn słynie z pięknych pierścieni.</p>
+        </section>
+      )}
     </>
   );
 };
