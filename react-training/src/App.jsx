c762463b0d7636c69b24f7ab5e19a09a217bc5f4
Katarzyna Tchir <tchir.katarzyna@gmail.com>
2025-02-19 14:41:04 +0100

    done

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+
 const App = () => {
-  const addName = (e) => {
+  const [names, setNames] = useState([]);
+  const [name, setName] = useState('');
+  const addName = e => {
     e.preventDefault();
+    if (name === '') return;
+    setNames([...names, name]);
+    setName('');
     // Dodaj logikę dodawania imienia do listy
   };
 
   return (
     <div>
       <h1>Lista imion użytkowników</h1>
-      <form onSubmit={addName}></form>
-      <ul>{/* Wyrenderuj tutaj listę imion */}</ul>
+      <form onSubmit={addName}>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="Dodaj nowe imię"
+        />
+        <button type="submit">Dodaj</button>
+      </form>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
